ac6f359884b043ea30017c3b7a12d0c5292c3658
Andrea <andrea_1996_melli@hotmail.com>
2026-06-16 17:30:14 +0200

    feature: semaforo

--- ui5-orders-app/webapp/controller/orders.controller.js
+++ ui5-orders-app/webapp/controller/orders.controller.js
@@ -8,12 +8,22 @@ sap.ui.define([
 
         onInit: function () {
             var oModel = new ODataModel({
-    serviceUrl: "/odata/v4/orders/",
-    synchronizationMode: "None",
-    operationMode: "Server"
-});
+                serviceUrl: "/odata/v4/orders/",
+                synchronizationMode: "None",
+                operationMode: "Server"
+            });
 
             this.getView().setModel(oModel);
+        },
+
+        formatStatus: function (sStatus) {
+            switch (sStatus) {
+                case "Delivered":  return "Success";
+                case "Shipped":    return "Information";
+                case "Pending":    return "Warning";
+                case "Cancelled":  return "Error";
+                default:           return "None";
+            }
         }
 
     });
